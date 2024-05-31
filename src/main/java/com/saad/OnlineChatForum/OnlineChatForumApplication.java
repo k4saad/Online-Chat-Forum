b8a9885410c38efee3d99cf3ba025b0ac414c371
saad khan <saadkhan108b@gmail.com>
2024-05-31 11:52:30 +0530

    first commit

---
+++ src/main/java/com/saad/OnlineChatForum/OnlineChatForumApplication.java
@@ -0,0 +1,13 @@
+package com.saad.OnlineChatForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineChatForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineChatForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saad/OnlineChatForum/OnlineChatForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saad.OnlineChatForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineChatForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
